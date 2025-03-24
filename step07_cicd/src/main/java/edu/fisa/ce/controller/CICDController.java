2845b4d99ae48138d6d25cb9a51c3747b08e6c65
BlueRedOrange <jmg0916789@gmail.com>
2025-03-24 10:36:04 +0900

    task1. project push

---
+++ step07_cicd/src/main/java/edu/fisa/ce/controller/CICDController.java
@@ -0,0 +1,16 @@
+package edu.fisa.ce.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class CICDController {
+	@GetMapping("fisa1")
+	public String reqRes() {
+		System.out.println("reqRes()********");
+		return "요청 응답 성공";
+	}
+	
+}
