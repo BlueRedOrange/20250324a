e47252b2905ff751fe96e226d1877e9818b1b965
BlueRedOrange <jmg0916789@gmail.com>
2025-04-09 09:45:35 +0900

    feat: test src

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -1,5 +1,7 @@
 package edu.fisa.ce;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,10 @@ class Step07CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int result = 2 +3;
+		System.out.println("**********비교전");
+		assertEquals(5,result);
+		System.out.println("**********비교전");
 	}
 
 }
