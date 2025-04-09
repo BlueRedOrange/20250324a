23e0c81068aebf3613ad3b3a658b0ed89ea40ab3
BlueRedOrange <jmg0916789@gmail.com>
2025-04-09 09:47:39 +0900

    fix: syso

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -13,7 +13,7 @@ class Step07CicdApplicationTests {
 		int result = 2 +3;
 		System.out.println("**********비교전");
 		assertEquals(5,result);
-		System.out.println("**********비교전");
+		System.out.println("**********비교후");
 	}
 
 }
